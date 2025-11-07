42de4f5ec3b9a4b1d32148688b9cebd2c8f29fca
Ivan Garcia <practica1web@grupofarmaceutico.com>
2025-11-07 16:49:24 +0100

    .

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy,Component,signal } from "@angular/core";
+
+
+@Component({
+  templateUrl:'./counter-page.component.html',
+  styles:`
+
+  button{
+  padding:5px;
+  margin: 5px 10px;
+  width:75px
+  }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CounterPagesComponent{
+
+  counter=10;
+  counterSignal = signal(10);
+
+  constructor(){
+    // setInterval(()=>{
+    // this.counter+=1
+    //   this.counterSignal.update((v)=> v+1);
+    //   console.log('Tick');
+    // } ,2000)
+  }
+
+  increasyBy(value:number){
+    this.counter +=value;
+     this.counterSignal.update((current)=> current + value);
+
+  }
+  decreaseBy(value:number){
+    this.counter -=value;
+    // this.counterSignal.set(this.counterSignal()+value)
+    this.counterSignal.update((current)=> current - value);
+  }
+
+  resetCounter(){
+    this.counter=0;
+    this.counterSignal.set(0);
+  }
+}
